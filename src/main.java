e63f8c935da79ccd4a9cd6df2abd0a7014b0087c
Danny'sCraptop <danwardvs@adsgames.net>
2016-03-11 08:46:27 -0500

    Origin is center of the screen

--- src/main.java
+++ src/main.java
@@ -20,10 +20,11 @@ public class main {
 	    // init OpenGL
 	    GL11.glMatrixMode(GL11.GL_PROJECTION);
 	    GL11.glLoadIdentity();
-	    GL11.glOrtho(0, 800, 0, 600, 1, -1);
+	    GL11.glOrtho(0,800, 0, 600, 1, -1);
 	    GL11.glMatrixMode(GL11.GL_MODELVIEW);
-	  
+	    GL11.glTranslatef(400, 300, 0);
 	    while (!Display.isCloseRequested()) {
+	    	
 	        // Clear the screen and depth buffer
 	        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
 	         
@@ -32,10 +33,10 @@ public class main {
 	             
 	        // draw quad
 	        GL11.glBegin(GL11.GL_QUADS);
-	            GL11.glVertex2f(100,100);
-	        GL11.glVertex2f(100+200,100);
-	        GL11.glVertex2f(100+200,100+200);
-	        GL11.glVertex2f(100,100+200);
+	            GL11.glVertex2f(0,0);
+	        GL11.glVertex2f(200,0);
+	        GL11.glVertex2f(200,200);
+	        GL11.glVertex2f(0,200);
 	        GL11.glEnd();
 	  
 	        Display.update();
