e5eeaff57635dd035f7c83a82f0dd0f4e4018145
danwardvs <danwardvs@adsgames.net>
2016-03-13 12:31:34 -0400

    Projectile class inherited from box

---
+++ src/Projectile.java
@@ -0,0 +1,21 @@
+
+
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.World;
+
+public class Projectile extends Box {
+	
+	float time;
+	float lifetime;
+	
+	public Projectile(World newWorld, BodyType newBodyType, float newX, float newY, float newWidth, float newHeight,
+			float newAngle, float newR, float newG, float newB, float newA, float newLifetime) {
+		super(newWorld, newBodyType, newX, newY, newWidth, newHeight, newAngle, newR, newG, newB, newA);
+		// TODO Auto-generated constructor stub
+		lifetime = newLifetime;
+	}
+	public void update(){
+		
+	}
+
+}
