6a78d40a1af551d5d54ccb8e0441b0b255f880f6
Danny'sCraptop <danwardvs@adsgames.net>
2016-03-22 09:15:05 -0400

    Adding a level class

--- src/WorldController.java
+++ src/WorldController.java
@@ -124,8 +124,8 @@ public class WorldController {
 		    // Run loop
 		    for (int i = 0; i <BOX_AMOUNT; ++i) {
 		    	
-		    	//gameBoxes.add(new Box(gameWorld,BodyType.DYNAMIC,(float)(Math.random()*30)-10,(float)(Math.random()*30)-15,(float)(Math.random()*2),(float)(Math.random()*2),(float)(Math.random()*2),1,1f,0.5f,0));
-		    	gameBoxes.add(new Box(gameWorld,BodyType.DYNAMIC,(i)-12,-13,0.2f,1,0,1,1f,0.5f,0));
+		    	gameBoxes.add(new Box(gameWorld,BodyType.DYNAMIC,(float)(Math.random()*30)-10,(float)(Math.random()*30)-15,(float)(Math.random()*2),(float)(Math.random()*2),(float)(Math.random()*2),1,1f,0.5f,0));
+		    	//gameBoxes.add(new Box(gameWorld,BodyType.DYNAMIC,(i)-12,-13,0.2f,1,0,1,1f,0.5f,0));
 		    	
 		    } 
 		    gameCharacter = new Character(gameController,gameWorld,-13,-2,0.5f,1.5f,0,0,1f,0,0);
